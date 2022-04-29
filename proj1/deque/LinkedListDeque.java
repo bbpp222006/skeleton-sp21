a98288e89f5a094b486f15b9445c4e2c6dfec5c3
bbpp222006 <34976166+bbpp222006@users.noreply.github.com>
2022-04-29 13:49:20 +0800

    upd
    pr1

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,99 @@
+package deque;
+
+public class LinkedListDeque<shit_things> {
+
+    private IntNode sentinel;
+
+    private int size;
+
+    public class IntNode {
+        public IntNode prev;
+        public shit_things item;
+        public IntNode next;
+        public IntNode(IntNode p,shit_things i, IntNode n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    public LinkedListDeque() {
+        size = 0;
+        sentinel= new IntNode(null,null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addLast(shit_things x) {
+        IntNode p = sentinel;
+
+        IntNode new_node = new IntNode(p.prev, x, p);
+        p.prev.next = new_node;
+        p.prev=new_node;
+        size += 1;
+    }
+
+    public void addFirst(shit_things x) {
+        IntNode p = sentinel;
+
+        IntNode new_node = new IntNode(p, x, p.next);
+        p.next.prev = new_node;
+        p.next=new_node;
+        size += 1;
+    }
+
+    public shit_things removeFirst() {
+        IntNode p = sentinel;
+        IntNode old_first_node = p.next;
+        IntNode new_first_node = old_first_node.next;
+        p.next = new_first_node;
+        new_first_node.prev=p;
+        if (size>0){
+            size -= 1;
+        }
+        return old_first_node.item;
+
+    }
+
+    public shit_things removeLast() {
+        IntNode p = sentinel;
+        IntNode old_last_node = p.prev;
+        IntNode new_last_node = old_last_node.prev;
+        p.prev = new_last_node;
+        new_last_node.next=p;
+        if (size>0){
+            size -= 1;
+        }
+
+        return old_last_node.item;
+    }
+
+    public shit_things get(int i) {
+
+        IntNode p = sentinel;
+
+        for (int j=0;j<=i;j++){
+            p = p.next;
+        }
+        return p.item;
+    }
+
+
+    public boolean isEmpty() {
+        return size == 0;
+
+    }
+
+    public void printDeque() {
+        IntNode p = sentinel;
+        while (p.next.item!=null){
+            System.out.print(p.next.item+" ");
+            p = p.next;
+        }
+    }
+
+}
